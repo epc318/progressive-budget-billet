102fb828e7a53f125b39a130fa4efc817bbf7f74
Rick Johnson <carl4917@umn.edu>
2022-03-12 19:22:36 -0600

    beginning funtins for offline usage in idb.js

---
+++ public/js/idb.js
@@ -0,0 +1,29 @@
+let db;
+const request = indexedDB.open('budget_tracker', 1)
+
+
+request.onupgradeneeded = function(event) {
+    const db = event.target.result;
+    db.createObjectStore('new_budget', { autoIncrement: true });
+};
+
+
+request.onsuccess = function(event) {
+    db = event.target.result;
+    if(navigator.onLine) {
+    }
+};
+
+
+//error log in the event of an error
+request.onerror = function(event) {
+    console.log(event.target.errorCode);
+};
+
+
+function saveRecord(record) {
+ 
+    const transaction = db.transaction(['new_budget'], 'readwrite');
+    const budgetObjectStore = transaction.objectStore('new_budget');
+    budgetObjectStore.add(record);
+};
