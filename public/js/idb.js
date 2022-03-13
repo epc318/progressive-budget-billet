6132d91224d40e1872969f0979c10c6e24986fd4
Rick Johnson <carl4917@umn.edu>
2022-03-12 19:48:08 -0600

    add object store eventlistener for online app connection

--- public/js/idb.js
+++ public/js/idb.js
@@ -27,3 +27,39 @@ function saveRecord(record) {
     const budgetObjectStore = transaction.objectStore('new_budget');
     budgetObjectStore.add(record);
 };
+
+
+function uploadBudget() {
+    const transaction = db.transaction(['new_budget'], 'readwrite');
+    const budgetObectStore = transaction.objectStore('new_budget');
+    const getAll = budgetObectStore.getAll();
+  
+    getAll.onsuccess = function() {
+      if (getAll.result.length > 0) {
+        fetch('/api/transaction/bulk', {
+          method: 'POST',
+          body: JSON.stringify(getAll.result),
+          headers: {
+            Accept: 'application/json, text/plain, */*',
+            'Content-Type': 'application/json'
+          }
+        })
+          .then(response => response.json())
+          .then(serverResponse => {
+            if (serverResponse.message) {
+              throw new Error(serverResponse);
+            }
+  
+            const transaction = db.transaction(['new_budget'], 'readwrite');
+            const budgetObectStore = transaction.objectStore('new_budget');
+            budgetObectStore.clear();
+          })
+          .catch(err => {
+            console.log(err);
+          });
+      }
+    };
+  }
+
+
+window.addEventListener('online', uploadBudget);
